1c2ea3ae335b8015958a4defe008b2eec3835a4f
roisd <Openthed00r>
2022-06-19 13:35:43 +0700

    perbaikan di SplashActivity.kt handler

--- app/src/main/java/com/admaja/myfirstsubmission/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/admaja/myfirstsubmission/ui/activity/SplashActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import android.view.WindowManager
 import com.admaja.myfirstsubmission.databinding.ActivitySplashBinding
 
@@ -20,7 +21,7 @@ class SplashActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
 
-        Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this@SplashActivity, MainActivity::class.java)
             startActivity(intent)
             finish()
